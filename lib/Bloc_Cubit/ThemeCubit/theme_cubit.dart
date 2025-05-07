95b4fa3d1419f196c61c438daee860d99b8224b7
ColemanKokotti <calin.coleasa@edu.itspiemonte.it>
2025-05-07 10:34:49 +0100

    theme fix

--- lib/Bloc_Cubit/ThemeCubit/theme_cubit.dart
+++ lib/Bloc_Cubit/ThemeCubit/theme_cubit.dart
@@ -1,7 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:shop_n_roll/Themes/godofwar_theme.dart';
 import 'package:shop_n_roll/Themes/honkaistarrail_theme.dart';
 import 'package:shop_n_roll/Themes/pokemon_theme.dart';
 import 'package:shop_n_roll/Themes/thewitcher_theme.dart';
@@ -9,9 +8,10 @@ import 'package:shop_n_roll/Themes/warframe_theme.dart';
 import '../../FireBase/theme_preference_service.dart';
 import '../../Themes/default_theme.dart';
 import '../../Themes/earthy_theme.dart';
+import '../../Themes/kingdom_theme.dart';
 import '../../Themes/light_theme.dart';
+import '../../Themes/mint_theme.dart';
 import '../../Themes/ocean_breeze_theme.dart';
-import '../../Themes/pastel_theme.dart';
 import '../../Themes/theme_dark.dart';
 import '../../Themes/vintageRetro_theme.dart';
 
@@ -19,11 +19,11 @@ final themeMap = {
   'default': defaultTheme,
   'light': lightTheme,
   'dark': darkTheme,
-  'pastel': pastelTheme,
+  'mint': mintTheme,
   'vintage': vintageRetroTheme,
   'earthy': earthyTheme,
   'ocean': oceanBreezeTheme,
-  'godofwar': godOfWarTheme,
+  'kingdom': kingdomHeartsTheme,
   'pokemon': pokemonTheme,
   'honkaistarrail': honkaiStarRailTheme,
   'warframe': warframeTheme,
